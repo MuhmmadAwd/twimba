a780f6663ac2baa35cc2ae7ee66c8101dd8878fb
Muhmmad Awd <muhmmadawd@gmail.com>
2023-03-14 21:23:18 +0200

    make the icon work #3

--- js/index.js
+++ js/index.js
@@ -11,7 +11,8 @@ function init() {
 function getFeedTweets() {
     let feedTweets = "";
     tweetsData.forEach((tweet) => {
-        
+        const iconRedClass = tweet.isLiked ? "icon-red" : ""
+        const iconGreenClass = tweet.isRetweeted ? "icon-green" : ""
 
         const tweetEl = `
         <article class="feed__tweet">
@@ -21,8 +22,8 @@ function getFeedTweets() {
             <p class="feed__body">${tweet.tweetText}</p>
             <ul class="feed_icon-list">
                 <li class="feed-icon"><i class="fa fa-commenting" data-id="${tweet.uuid}" data-icon="reply"></i> ${tweet.replies.length}</li>
-                <li class="feed-icon"><i class="fa fa-heart " data-id="${tweet.uuid}" data-icon="like"></i> ${tweet.likes}</li>
-                <li class="feed-icon"><i class="fa fa-retweet " data-id="${tweet.uuid}" data-icon="retweet"></i> ${tweet.retweets}</li>
+                <li class="feed-icon"><i class="fa fa-heart ${iconRedClass}" data-id="${tweet.uuid}" data-icon="like"></i> ${tweet.likes}</li>
+                <li class="feed-icon"><i class="fa fa-retweet ${iconGreenClass}" data-id="${tweet.uuid}" data-icon="retweet"></i> ${tweet.retweets}</li>
             </ul>
             </div>
         </article>
@@ -36,5 +37,28 @@ function renderFeedTweets() {
     document.querySelector(".feed").innerHTML = getFeedTweets();
 }
 
+function handleTweetsData(tweetTarget) {
+    const tweetId = tweetTarget.dataset.id;
+    const tweetIcon = tweetTarget.dataset.icon;
+    tweetsData.forEach((tweet) => {
+        if (tweet.uuid == tweetId) {
+            if (tweetIcon == "like") {
+                tweet.isLiked ? tweet.likes -= 1 : tweet.likes += 1
+                tweet.isLiked = !tweet.isLiked
+            } else if (tweetIcon == "retweet") {
+                tweet.isRetweeted ? tweet.retweets -= 1 : tweet.retweets += 1
+                tweet.isRetweeted = !tweet.isRetweeted
+            }
+        }
+        renderFeedTweets()
+    })
+}
 
+function handleClick(e) {
+    const tweetTarget = e.target;
+    if (tweetTarget.dataset.id) {
+        handleTweetsData(tweetTarget)
+    }
+
+}
 document.addEventListener("DOMContentLoaded", init)
